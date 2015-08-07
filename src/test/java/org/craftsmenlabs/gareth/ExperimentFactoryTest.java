054501ba5ada330b8e8738eae47b3411a841da03
Hylke Stapersma <hylke.stapersma@gmail.com>
2015-08-07 13:38:34 +0200

    Added experiment 4 test (success) added

--- src/test/java/org/craftsmenlabs/gareth/ExperimentFactoryTest.java
+++ src/test/java/org/craftsmenlabs/gareth/ExperimentFactoryTest.java
@@ -84,6 +84,26 @@ public class ExperimentFactoryTest {
 
     }
 
+    @Test
+    public void testExperiment4() {
+        final Experiment experiment = parseTestExperiment("experiment-0004.experiment");
+
+
+        assertNotNull(experiment);
+        assertEquals("Reduce failed logins (with success)", experiment.getExperimentName());
+        assertEquals(1, experiment.getAssumptionBlockList().size());
+        // Validate assumption block
+        final AssumptionBlock assumptionBlock = experiment.getAssumptionBlockList().get(0);
+        final AssumptionBlock expectedAssumptionBlock =  new AssumptionBlock();
+        expectedAssumptionBlock.setBaseline("Failed logins are read from log");
+        expectedAssumptionBlock.setAssumption("By showing a caps-lock warning the failed logins are reduced by 5%");
+        expectedAssumptionBlock.setTime("5 days");
+        expectedAssumptionBlock.setSuccess("this is a success");
+
+        validateAssumptionBlock(assumptionBlock, expectedAssumptionBlock);
+
+    }
+
     private Experiment parseTestExperiment(final String fileName) {
         final InputStream fileInputStream = getClass().getResourceAsStream("/experiments/" + fileName);
 
