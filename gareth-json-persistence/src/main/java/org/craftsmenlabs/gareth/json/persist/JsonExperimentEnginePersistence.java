508390b9edcd8425b51818a9241872e66c37fb92
Hylke Stapersma <hylke.stapersma@gmail.com>
2016-01-14 11:13:12 +0100

    Splitted out JSON persistence into StorageMedia and StorageEngine

--- gareth-json-persistence/src/main/java/org/craftsmenlabs/gareth/json/persist/JsonExperimentEnginePersistence.java
+++ gareth-json-persistence/src/main/java/org/craftsmenlabs/gareth/json/persist/JsonExperimentEnginePersistence.java
@@ -1,12 +1,5 @@
 package org.craftsmenlabs.gareth.json.persist;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.AnnotationIntrospector;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
-import org.apache.commons.io.IOUtils;
 import org.craftsmenlabs.gareth.api.ExperimentEngine;
 import org.craftsmenlabs.gareth.api.context.ExperimentContext;
 import org.craftsmenlabs.gareth.api.context.ExperimentRunContext;
@@ -15,15 +8,13 @@ import org.craftsmenlabs.gareth.api.exception.GarethStateWriteException;
 import org.craftsmenlabs.gareth.api.exception.GarethUnknownExperimentException;
 import org.craftsmenlabs.gareth.api.listener.ExperimentStateChangeListener;
 import org.craftsmenlabs.gareth.api.persist.ExperimentEnginePersistence;
-import org.craftsmenlabs.gareth.api.storage.Storage;
 import org.craftsmenlabs.gareth.core.context.ExperimentRunContextImpl;
 import org.craftsmenlabs.gareth.json.persist.listener.JsonExperimentChangeListener;
-import org.craftsmenlabs.gareth.json.persist.serializer.StorageDeserializer;
-import org.craftsmenlabs.gareth.json.persist.serializer.StorageSerializer;
+import org.craftsmenlabs.gareth.json.persist.media.StorageMedia;
+import org.craftsmenlabs.gareth.json.persist.media.file.FileStorageMedia;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,12 +25,12 @@ public class JsonExperimentEnginePersistence implements ExperimentEnginePersiste
 
     private static final Logger LOG = LoggerFactory.getLogger(JsonExperimentEnginePersistence.class);
 
-    private final File stateFile;
+    private final StorageMedia storageMedia;
 
     private final JsonExperimentChangeListener jsonExperimentChangeListener;
 
     private JsonExperimentEnginePersistence(final Builder builder) {
-        this.stateFile = builder.stateFile;
+        this.storageMedia = builder.storageMedia;
         jsonExperimentChangeListener = new JsonExperimentChangeListener.Builder(this).build();
     }
 
@@ -50,39 +41,13 @@ public class JsonExperimentEnginePersistence implements ExperimentEnginePersiste
             data.add(assembleToContextData(experimentRunContext));
         }));
 
-        final ObjectMapper objectMapper = getObjectMapper();
-
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(stateFile);
-            objectMapper.writeValue(fos, data);
-        } catch (final FileNotFoundException e) {
-            LOG.error("File cannot be found", e);
-            throw new GarethStateWriteException(e);
-        } catch (final IOException e) {
-            LOG.error("Error while writing experiment engine data", e);
-            throw new GarethStateWriteException(e);
-        } finally {
-            IOUtils.closeQuietly(fos);
-        }
+        storageMedia.persist(data);
     }
 
-    private ObjectMapper getObjectMapper() {
-        final ObjectMapper objectMapper = new ObjectMapper();
-        final SimpleModule simpleModule = new SimpleModule();
-        simpleModule.addSerializer(Storage.class, new StorageSerializer());
-        simpleModule.addDeserializer(Storage.class, new StorageDeserializer());
-        //Register modules
-        objectMapper.registerModule(simpleModule);
-        objectMapper.registerModule(new JavaTimeModule());
-        final AnnotationIntrospector introspector = new JaxbAnnotationIntrospector(objectMapper.getTypeFactory());
-        objectMapper.setAnnotationIntrospector(introspector);
-        return objectMapper;
-    }
 
     @Override
     public void restore(final ExperimentEngine experimentEngine) throws GarethStateReadException {
-        final List<JsonExperimentContextData> experimentContextDataList = readExperimentContextDataFromFile();
+        final List<JsonExperimentContextData> experimentContextDataList = storageMedia.restore();
 
         experimentContextDataList.forEach(experimentContextData -> {
             try {
@@ -109,25 +74,6 @@ public class JsonExperimentEnginePersistence implements ExperimentEnginePersiste
         experimentRunContext.setFailureRun(experimentContextData.getFailureRun());
 
         return experimentRunContext;
-    }
-
-    private List<JsonExperimentContextData> readExperimentContextDataFromFile() throws GarethStateReadException {
-        List<JsonExperimentContextData> jsonExperimentContextDataList = null;
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(stateFile);
-            jsonExperimentContextDataList = getObjectMapper().readValue(fis, new TypeReference<List<JsonExperimentContextData>>() {
-            });
-        } catch (final FileNotFoundException e) {
-            LOG.error("File cannot be found", e);
-            throw new GarethStateReadException(e);
-        } catch (final IOException e) {
-            LOG.error("Exception while reading file", e);
-            throw new GarethStateReadException(e);
-        } finally {
-            IOUtils.closeQuietly(fis);
-        }
-        return jsonExperimentContextDataList;
     }
 
 
@@ -159,32 +105,15 @@ public class JsonExperimentEnginePersistence implements ExperimentEnginePersiste
 
     public static class Builder {
 
-        private final static String STATE_FILENAME = "gareth.state.json";
-        private final static String TMP_DIR = System.getProperty("java.io.tmpdir");
+        private StorageMedia storageMedia = new FileStorageMedia.Builder().build();
 
-        private File stateFile;
-
-        public Builder setStateFile(final File stateFile) {
-            this.stateFile = stateFile;
+        public Builder setStorageMedia(final StorageMedia storageMedia) {
+            this.storageMedia = storageMedia;
             return this;
         }
 
         public ExperimentEnginePersistence build() {
-            setupStateFile();
             return new JsonExperimentEnginePersistence(this);
-        }
-
-        private void setupStateFile() {
-            if (null == stateFile) {
-                stateFile = new File(TMP_DIR, STATE_FILENAME);
-            }
-            try {
-                if (!stateFile.exists()) {
-                    stateFile.createNewFile();
-                }
-            } catch (final IOException e) {
-                throw new IllegalStateException(String.format("Cannot setup state file %s", stateFile.getPath()), e);
-            }
         }
     }
 }
--- gareth-json-persistence/src/test/java/org/craftsmenlabs/gareth/json/persist/JsonExperimentEnginePersistenceTest.java
+++ gareth-json-persistence/src/test/java/org/craftsmenlabs/gareth/json/persist/JsonExperimentEnginePersistenceTest.java
@@ -3,18 +3,27 @@ package org.craftsmenlabs.gareth.json.persist;
 import org.apache.commons.io.IOUtils;
 import org.craftsmenlabs.gareth.api.ExperimentEngine;
 import org.craftsmenlabs.gareth.api.context.ExperimentContext;
+import org.craftsmenlabs.gareth.api.context.ExperimentRunContext;
+import org.craftsmenlabs.gareth.api.exception.GarethStateReadException;
 import org.craftsmenlabs.gareth.api.exception.GarethStateWriteException;
 import org.craftsmenlabs.gareth.api.listener.ExperimentStateChangeListener;
 import org.craftsmenlabs.gareth.api.persist.ExperimentEnginePersistence;
+import org.craftsmenlabs.gareth.core.context.ExperimentRunContextImpl;
+import org.craftsmenlabs.gareth.json.persist.media.StorageMedia;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
@@ -33,72 +42,61 @@ public class JsonExperimentEnginePersistenceTest {
     @Mock
     private ExperimentContext mockExperimentContext;
 
-    private File randomTmpFile;
+    @Mock
+    private StorageMedia mockStorageMedia;
+
+    @Mock
+    private ExperimentRunContext mockExperimentRunContext;
+
+    @Captor
+    private ArgumentCaptor<List<JsonExperimentContextData>> argumentCaptor;
+
 
     @Before
-    public void setUp() throws Exception {
+    public void before() {
         MockitoAnnotations.initMocks(this);
-        when(mockExperimentEngine.findExperimentContextForHash(anyString())).thenReturn(mockExperimentContext);
-
-        jsonExperimentEnginePersistence = new JsonExperimentEnginePersistence.Builder().build();
-
-        randomTmpFile = File.createTempFile("gareth-random", "state.json");
-        randomTmpFile.deleteOnExit();
+        when(mockExperimentEngine.getExperimentRunContexts()).thenReturn(asList(mockExperimentRunContext));
+        jsonExperimentEnginePersistence = new JsonExperimentEnginePersistence
+                .Builder()
+                .setStorageMedia(mockStorageMedia)
+                .build();
     }
 
-    @After
-    public void after() {
-        deleteFileAfterTest(randomTmpFile);
-    }
-
-    private void deleteFileAfterTest(final File fileToDelete) {
-        while (!fileToDelete.exists()) {
-            fileToDelete.delete();
-        }
-    }
-
-    @Test
-    public void testBuildWithStateFile() {
-        new JsonExperimentEnginePersistence.Builder().setStateFile(randomTmpFile).build();
-        assertTrue(randomTmpFile.exists());
-    }
-
-    @Test
-    public void testBuildWithCorruptStateFile() {
-        try {
-            final File corruptFile = mock(File.class);
-            when(corruptFile.getPath()).thenReturn("/path");
-            when(corruptFile.exists()).thenThrow(IOException.class);
-            new JsonExperimentEnginePersistence.Builder().setStateFile(corruptFile).build();
-            fail("Should not reach this point");
-        } catch (final IllegalStateException e) {
-            assertTrue(e.getMessage().contains("Cannot setup state file /path"));
-        }
-    }
 
     @Test
     public void testPersist() throws Exception {
         jsonExperimentEnginePersistence.persist(mockExperimentEngine);
-        verify(mockExperimentEngine).getExperimentRunContexts();
-    }
-
-    @Test
-    public void testBuildWithNewFile() throws Exception {
-        final Random random = new Random();
-        final int randomNumber = random.nextInt();
-        final File newFile = new File("unknown" + randomNumber);
-        newFile.deleteOnExit();
-        jsonExperimentEnginePersistence = new JsonExperimentEnginePersistence.Builder().setStateFile(newFile).build();
-        assertTrue(newFile.exists());
-        deleteFileAfterTest(newFile);
+        verify(mockStorageMedia).persist(argumentCaptor.capture());
+        assertEquals(1, argumentCaptor.getValue().size());
+        assertTrue(argumentCaptor.getValue().get(0) instanceof JsonExperimentContextData);
     }
 
     @Test
     public void testRestore() throws Exception {
-        jsonExperimentEnginePersistence = new JsonExperimentEnginePersistence.Builder().setStateFile(randomTmpFile).build();
-        copyDataToTmpFile();
         jsonExperimentEnginePersistence.restore(mockExperimentEngine);
-        verify(mockExperimentEngine, times(3)).findExperimentContextForHash(anyString());
+        verify(mockStorageMedia).restore();
+    }
+
+    @Test
+    public void testPersistWithGarethWriteException() throws Exception {
+        try {
+            doThrow(GarethStateWriteException.class).when(mockStorageMedia).persist(anyList());
+            jsonExperimentEnginePersistence.persist(mockExperimentEngine);
+            fail("Should not reach this point");
+        } catch (final GarethStateWriteException e) {
+
+        }
+    }
+
+    @Test
+    public void testRestoreWithGarethReadException() throws Exception {
+        try {
+            when(mockStorageMedia.restore()).thenThrow(GarethStateReadException.class);
+            jsonExperimentEnginePersistence.restore(mockExperimentEngine);
+            fail("Should not reach this point");
+        } catch (final GarethStateReadException e) {
+
+        }
     }
 
     @Test
@@ -108,16 +106,5 @@ public class JsonExperimentEnginePersistenceTest {
         assertSame(experimentStateChangeListener1, experimentStateChangeListener2);
     }
 
-    private void copyDataToTmpFile() throws IOException {
-        FileOutputStream output = null;
-        InputStream input = null;
-        try {
-            output = new FileOutputStream(randomTmpFile);
-            input = getClass().getResourceAsStream("/sample/test-state-gareth.json");
-            IOUtils.copy(input, output);
-        } finally {
-            IOUtils.closeQuietly(input);
-            IOUtils.closeQuietly(output);
-        }
-    }
+
 }
