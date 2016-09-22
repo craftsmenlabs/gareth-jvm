1daa18dde1bcca74c4104c7b497f9a735ac42e4f
David Hardy <david.hardy@codecentric.nl>
2016-09-22 14:47:45 +0200

    Removed example class and use parameter

--- gareth-application/src/main/java/org/craftsmenlabs/gareth/application/ApplicationContainer.java
+++ gareth-application/src/main/java/org/craftsmenlabs/gareth/application/ApplicationContainer.java
@@ -1,27 +1,20 @@
 package org.craftsmenlabs.gareth.application;
 
-import org.craftsmenlabs.gareth.api.model.AssumptionBlock;
-import org.craftsmenlabs.gareth.api.model.Experiment;
 import org.craftsmenlabs.gareth.core.ExperimentEngine;
-import org.craftsmenlabs.gareth.core.ExperimentEngineConfig;
-import org.craftsmenlabs.gareth.rest.RestServiceFactory;
 import org.craftsmenlabs.gareth.rest.RestService;
+import org.craftsmenlabs.gareth.rest.RestServiceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class ApplicationContainer {
 
-    private ExperimentEngineConfig experimentEngineConfig;
     private ExperimentEngine experimentEngine;
 
     @Autowired
-    public ApplicationContainer(ExperimentEngineConfig experimentEngineConfig, ExperimentEngine experimentEngine) {
-        this.experimentEngineConfig = experimentEngineConfig;
+    public ApplicationContainer(ExperimentEngine experimentEngine) {
         this.experimentEngine = experimentEngine;
     }
 
@@ -35,25 +28,6 @@ public class ApplicationContainer {
         final RestServiceFactory restServiceFactory = new RestServiceFactory(); // Create a new rest service factory
         final RestService restService = restServiceFactory.create(experimentEngine, 8888);
         restService.start();
-    }
-
-    private static Experiment createExperiment() {
-        Experiment experiment = new Experiment();
-        experiment.setExperimentName("standalone");
-        experiment.setAssumptionBlockList(createAssumptionBlock());
-        return experiment;
-    }
-
-    private static List<AssumptionBlock> createAssumptionBlock() {
-        ArrayList<AssumptionBlock> blocks = new ArrayList<>();
-        AssumptionBlock block = new AssumptionBlock();
-        block.setAssumption("has risen by 10 per cent");
-        block.setBaseline("sale of bananas");
-        block.setFailure("Blame the suits");
-        block.setTime("3 seconds");
-        block.setSuccess("send cake to developers");
-        blocks.add(block);
-        return blocks;
     }
 
     /**
