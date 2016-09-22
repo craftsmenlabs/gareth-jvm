49b9b07ca4ce7a9ad3dd98a62921a58e4328e333
David Hardy <david.hardy@codecentric.nl>
2016-09-22 14:36:36 +0200

    Removed rest v1

--- gareth-rest/src/main/java/org/craftsmenlabs/gareth/rest/GarethApplication.java
+++ gareth-rest/src/main/java/org/craftsmenlabs/gareth/rest/GarethApplication.java
@@ -3,11 +3,6 @@ package org.craftsmenlabs.gareth.rest;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import org.craftsmenlabs.gareth.core.ExperimentEngine;
 import org.craftsmenlabs.gareth.rest.binder.ExperimentEngineFeature;
-import org.craftsmenlabs.gareth.rest.filter.CORSFilter;
-import org.craftsmenlabs.gareth.rest.v1.resource.DefinitionsResource;
-import org.craftsmenlabs.gareth.rest.v1.resource.ExperimentRerunResource;
-import org.craftsmenlabs.gareth.rest.v1.resource.ExperimentResource;
-import org.craftsmenlabs.gareth.rest.v1.resource.ExperimentRunResource;
 import org.glassfish.jersey.linking.DeclarativeLinkingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -17,10 +12,5 @@ public class GarethApplication extends ResourceConfig {
         registerInstances(new ExperimentEngineFeature(experimentEngine));
         register(JacksonJaxbJsonProvider.class);
         register(DeclarativeLinkingFeature.class);
-        register(ExperimentResource.class);
-        register(ExperimentRunResource.class);
-        register(DefinitionsResource.class);
-        register(ExperimentRerunResource.class);
-        register(CORSFilter.class);
     }
 }
