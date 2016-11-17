5842bc3f51c04e298c24feb8586959a552bafd71
jroetman <janmartijn.roetman@codecentric.nl>
2016-11-17 14:13:58 +0100

    formatting

--- gareth-api/src/main/java/org/craftsmenlabs/gareth/api/annotation/Success.java
+++ gareth-api/src/main/java/org/craftsmenlabs/gareth/api/annotation/Success.java
@@ -1,11 +1,9 @@
 package org.craftsmenlabs.gareth.api.annotation;
 
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
