3efadee808eb772f57ce79ebef14dd9bef31e4f1
Jasper Sprengers <jasper.sprengers@codecentric.nl>
2017-04-04 09:32:27 +0200

    cleaned up dependency(management) in maven projects

--- gareth-execution-ri/src/main/kotlin/org/craftsmenlabs/gareth/execution/GarethExecutionConfig.kt
+++ gareth-execution-ri/src/main/kotlin/org/craftsmenlabs/gareth/execution/GarethExecutionConfig.kt
@@ -1,6 +1,7 @@
 package org.craftsmenlabs.gareth.execution
 
 import com.fasterxml.jackson.databind.ObjectMapper
+
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
